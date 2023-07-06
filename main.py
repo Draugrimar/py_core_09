a741fb98c39948a773cb0d4b82d7ce953f7d4ec8
Draugrimar <mr.sanscrit@gmail.com>
2023-07-06 06:11:16 +0300

    v 0.2.0

--- main.py
+++ main.py
@@ -1,55 +1,84 @@
-from func import parcer, add, change, phone, get_phone_book
+phone_book = {}
 
-def main():
 
-    def errors(error_name):                                         # Дабы не копировать эти строки по 5 раз, закинул их во внутреннюю функцию. Формально, принт всё еще в main :)
-        if error_name == "index":
-            print("Please enter command, name and phone number")
-        if error_name == "key":
-            print("No such name in the phone book")
-        if error_name == "value":
-            print("Please enter correct number")
-        if error_name == "done":
-            print("Completed!")
-        if error_name == "existed":
-            print("Such name already exists")
-
-    while True:                                                     # Основной блок
-
+def main():                                                 # Основной модуль
+    while True:
         user_input = input("Please enter your command: ")
-        parced = parcer(user_input)
-        errors(parced)
-        ps = parced.split(" ")
-
-        if ps[0] == "hello":
-            print("Hi! How can I help you?")
-
-        if ps[0] == "bye":
-            print("Good bye!")
+        result = parcer(user_input)
+        print(result)
+        if result == "Good bye!":
             break
-
-        if ps[0] == "add":
-            result = add(parced)
-            errors(result)
-
-        if ps[0] == "change":
-            result = change(parced)
-            errors(result)
-
-        if ps[0] == "phone":
-            result = phone(parced)
-            if result in ["index", "key", "value", "done"]:
-                errors(result)
-            else:
-                print(result)
-
-        if ps[0] == "show_all":
-            print(get_phone_book())
-
-        if ps[0] == "skip":
-            continue
-
 
 
 if __name__ == "__main__":
     main()
+
+
+
+def input_error(func):                                      # Декоратор ошибок
+    def inner_func(text):
+        try:
+            return func(text)
+        except IndexError:
+            return "Please enter command, name and phone number"
+        except KeyError:
+            return "No such name in the phone book"
+        except ValueError:
+            return "Please enter correct number"
+
+    return inner_func
+
+
+def normalize(text):
+    return text.lower()
+
+
+@input_error                                                    # Парсер команд
+def parcer(text):
+    line = text.split(" ")
+    command = normalize(line[0])
+    if command == "hello":
+        return "Hi! How can I help you?"
+    if command in ["good bye", "close", "exit"]:
+        return "Good bye!"
+    if command == "add":
+        return add(f"{command} {line[1]} {line[2]}")
+    if command == "change":
+        return change(f"{command} {line[1]} {line[2]}")
+    if command == "phone":
+        return phone(f"phone {line[1]}")
+    if command == "show" and normalize(line[1]) == "all":
+        return get_phone_book()
+    else:
+        return "Unknown command, please try again"
+
+
+@input_error
+def add(text):                                                  # Добавление новых контактов
+    text = text.split(" ")
+    if text[1] not in phone_book:
+        phone_book.update({text[1]: text[2]})
+        return "Completed!"
+    else:
+        return "Such name already exists"
+
+
+@input_error
+def change(text):                                               # Изменение существующих контактов
+    text = text.split(" ")
+    phone_book.pop(text[1])
+    phone_book.update({text[1]: text[2]})
+    return "Completed!"
+
+
+@input_error
+def phone(text):                                                # Достаём контак по имени
+    text = text.split(" ")
+    return phone_book[text[1]]
+
+
+def get_phone_book():                                           # Вывод всей телефонной книги
+    result = ""
+    for name, phone in phone_book.items():
+        result += "\n" + name + " " + phone
+    return result
